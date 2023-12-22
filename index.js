0de770dd8e73f00eed18b91902a4faba8a07afd1
raja563 <shakyaraja1998@gmail.com>
2023-12-22 19:14:57 +0530

    this is initila commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+this link will copy and create json file
+https://api.openweathermap.org/data/2.5/weather?q={kanpur}&appid={dea69d3815c2b5f78f80e7297cbe19bc}
+
+
+this is the error code 
+
+{"cod":401, "message": "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."}
+*/
